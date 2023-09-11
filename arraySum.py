43698b359154942981e5bf027bdb62201985dbe8
Harry <Ananypratap007@users.noreply.github.com>
2023-09-11 14:03:23 -0500

    multiplication thing added

--- arraySum.py
+++ arraySum.py
@@ -1,7 +1,16 @@
 def sum_list(numbers):
     return sum(numbers)
 
+def product_list(numbers):
+    product = 1
+    for num in numbers:
+        product *= num
+    return product
+
 # Example usage:
 ex_numbers = [1, 2, 3, 4, 5]
 result = sum_list(ex_numbers)
 print("Sum of the numbers:", result)
+
+result_product = product_list(ex_numbers)
+print("Product of the numbers:", result_product)
